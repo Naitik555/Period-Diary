3c387d78c4af518bbf7a3353c2011a8a481fbea7
Gurdeep <saini.gurdeep1688@gmail.com>
2021-02-22 13:41:19 +0530

    Third commit - splash animation

---
+++ app/src/main/java/com/project/perioddiary/splash/SplashActivity.java
@@ -0,0 +1,53 @@
+package com.project.perioddiary.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.project.perioddiary.IntroActivity;
+import com.project.perioddiary.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnim,bottomAnim;
+    //ImageView image;
+    TextView name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+      //  image = findViewById(R.id.imageview);
+          name = findViewById(R.id.name);
+
+          name.setAnimation(bottomAnim);
+
+        Thread myThread = new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
