b1945f0e39077ef8ec6b2fcfa23e23838db89cef
Naitik555 <79523695+Naitik555@users.noreply.github.com>
2021-03-06 20:37:38 +0530

    for image

---
+++ app/src/main/java/com/project/perioddiary/doctorpatient.java
@@ -0,0 +1,39 @@
+package com.project.perioddiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class doctorpatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctorpatient);
+
+        ImageView img = (ImageView) findViewById(R.id.doctor);
+        img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your code here
+                Intent intent = new Intent(doctorpatient.this, doctorlogin.class);
+                startActivity(intent);
+            }
+            });
+            ImageView img1 = (ImageView) findViewById(R.id.patient);
+        img1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your code here
+                Intent intent = new Intent(doctorpatient.this, patientlogin.class);
+                startActivity(intent);
+            }
+        });
+
+
+            }
+
+}
